2ce5820620659128eb45463adee64f885087fc1d
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2023-04-02 22:10:11 +0200

    Basic chat interface

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("main.js loaded.");
+
+const msgContainer = document.querySelector("#chat-messages");
+const msgInput = document.querySelector("#text-message");
+const sendBtn = document.querySelector("#chat-send");
+const alert = document.querySelector("#alert-container");
+const closeBtn = document.querySelector("#delete-message");
+
+sendBtn.addEventListener("click", (e) => {
+	const message = msgInput.value;
+	if (message.trim() === "") {
+		alert.style.visibility = "visible";
+	}
+});
+
+closeBtn.addEventListener("click", (e) => {
+	alert.style.visibility = "hidden";
+});
