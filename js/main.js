08e8e0adf16a96af481cca03baa9d4a741025636
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2023-04-02 22:23:22 +0200

    Basic chat finished

--- js/main.js
+++ js/main.js
@@ -10,9 +10,29 @@ sendBtn.addEventListener("click", (e) => {
 	const message = msgInput.value;
 	if (message.trim() === "") {
 		alert.style.visibility = "visible";
+		return;
 	}
+
+	const msgBox = createMessageBox(message);
+	msgContainer.innerHTML += msgBox;
+
+	msgInput.value = "";
 });
 
 closeBtn.addEventListener("click", (e) => {
 	alert.style.visibility = "hidden";
 });
+
+function createMessageBox(message) {
+	const box = `
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        ${message}
+                    </div>
+                </div>
+            </div>
+    `;
+
+	return box;
+}
