41d034df4bd53dab7708ead77644280fed4a7a16
Bouchtaoui <102304226+Bouchtaoui@users.noreply.github.com>
2023-04-02 23:30:57 +0200

    Improved chat interface

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const sendBtn = document.querySelector("#chat-send");
 const alert = document.querySelector("#alert-container");
 const closeBtn = document.querySelector("#delete-message");
 
+let toggle = true;
+
 sendBtn.addEventListener("click", (e) => {
 	const message = msgInput.value;
 	if (message.trim() === "") {
@@ -13,10 +15,22 @@ sendBtn.addEventListener("click", (e) => {
 		return;
 	}
 
-	const msgBox = createMessageBox(message);
+	// const msgBox = createMessageBox(message);
+	const msgBox = createMessage(message, toggle);
+
 	msgContainer.innerHTML += msgBox;
 
 	msgInput.value = "";
+
+	toggle = !toggle;
+});
+
+msgInput.addEventListener("keypress", (e) => {
+	if (e.key === "Enter") {
+		e.preventDefault();
+
+		sendBtn.click();
+	}
 });
 
 closeBtn.addEventListener("click", (e) => {
@@ -32,6 +46,20 @@ function createMessageBox(message) {
                     </div>
                 </div>
             </div>
+    `;
+
+	return box;
+}
+
+function createMessage(message, sending = true) {
+	const box = `
+            <article class="message ${sending ? "is-info" : "is-success"}">
+                <div class="message-body p-1">
+                    <h3><strong>Neo</strong></h3>
+                    <p class="py-1">${message}</p>
+                    <div class="is-flex"><span style="font-size:11px; font-weight: bold">${new Date().toLocaleTimeString()}</span></div>
+                </div>
+            </article>
     `;
 
 	return box;
